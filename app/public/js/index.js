906df9c0eb8ea47335e45e475fc817639b65985e
yutifan <yutifan@iu.edu>
2021-09-21 16:16:14 -0400

    vue

---
+++ app/public/js/index.js
@@ -0,0 +1,50 @@
+const Offer = {
+    data() {
+      return {
+        "person": {},
+        "offers": [
+                {
+                    "id": 1,
+                    "name": "Jane Student",
+                    "offer": 100000,
+                    "bonus": 9000,
+                    "company": "EY",
+                    "offerDate": "2021-10-05"
+                },
+                {
+                    "id": 2,
+                    "name": "Jordan Student",
+                    "offer": 87000,
+                    "bonus": 3000,
+                    "company": "IU",
+                    "offerDate": "2021-09-25"
+                }
+            ]
+        }
+    },
+    computed:{
+        prettyBirthday( ) {
+            return dayjs(this.person.dob.date)
+            .format ('YYYY-M-D')
+        }
+    },
+    created() {
+        console.log("A");
+
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((parsedJson) => {
+            console.log(parsedJson);
+            this.person = parsedJson.results[0]
+            console.log("C");
+        })
+        .catch( err => {
+            console.error(err)
+        })
+
+        console.log("B");
+    }
+  }
+  
+Vue.createApp(Offer).mount('#userApp');
+console.log("Z")
