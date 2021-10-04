822fe7548995cffa4a4c5140c5c9489342ec8bdb
yutifan <yutifan@iu.edu>
2021-10-04 10:16:35 -0400

    sql db

--- app/public/api/student/index.php
+++ app/public/api/student/index.php
@@ -6,7 +6,7 @@ require 'class/DbConnection.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM students';
+$sql = 'SELECT * FROM student';
 $vars = [];
 
 // if (isset($_GET['guid'])) {
@@ -18,10 +18,10 @@ $vars = [];
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$students = $stmt->fetchAll();
+$patients = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($students, JSON_PRETTY_PRINT);
+$json = json_encode($patients, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
